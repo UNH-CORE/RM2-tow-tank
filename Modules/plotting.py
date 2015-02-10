91be548c3827d1ea703d0cf01de54846d53d4265
Pete Bachant <petebachant@gmail.com>
2015-02-10 13:40:57 -0500

    Update perf Re dep plot function

--- Modules/plotting.py
+++ Modules/plotting.py
@@ -377,98 +377,57 @@ def plot_trans_wake_profile(quantity, U_infty=0.4, z_H=0.0, save=False, savedir=
     plt.tight_layout()
     
 def plot_perf_re_dep(save=False, savedir="Figures", savetype=".pdf", errorbars=False,
-                     cfd=False, normalize_by="default", dual_xaxes=False, show=True):
-    speeds = np.array([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3])
-    cp = np.zeros(len(speeds))
-    std_cp = np.zeros(len(speeds))
-    exp_unc_cp = np.zeros(len(speeds))
-    cd = np.zeros(len(speeds))
-    std_cd = np.zeros(len(speeds))
-    exp_unc_cd = np.zeros(len(speeds))
-    Re_D = speeds*D/1e-6
-    for n in range(len(speeds)):
-        if speeds[n] in [0.3, 0.5, 0.7, 0.9, 1.1, 1.3]:
-            section = "Perf-"+str(speeds[n])
-            df = pd.read_csv(os.path.join("Data", "Processed", section+".csv"))
-            cp_s = df.mean_cp
-            dcp_s = df.exp_unc_cp
-            cd_s = df.mean_cd
-            dcd_s = df.exp_unc_cd
-            cp[n] = np.mean(cp_s)
-            exp_unc_cp[n] = np.mean(dcp_s)
-            cd[n] = np.mean(cd_s)
-            exp_unc_cd[n] = np.mean(dcd_s)
-        else:
-            section = "Wake-"+str(speeds[n])
-            df = pd.read_csv(os.path.join("Data", "Processed", section+".csv"))
-            cp_s = df.mean_cp
-            dcp_s = df.exp_unc_cp
-            cd_s = df.mean_cd
-            dcd_s = df.exp_unc_cd
-            cp[n], std_cp[n] = np.mean(cp_s), np.std(cp_s)
-            cd[n], std_cd[n] = np.mean(cd_s), np.std(cd_s)
-            exp_unc_cp[n] = np.mean(dcp_s)
-            exp_unc_cd[n] = np.mean(dcd_s)
-    plt.figure()
+                     normalize_by="default", dual_xaxes=False, show=True):
+    df = pd.read_csv("Data/Processed/Perf-tsr_0.csv")
+    df = df.append(pd.read_csv("Data/Processed/Perf-tsr_0-b.csv"), ignore_index=True)
+    df = df[df.tow_speed_nom > 0.21]    
+    df = df.groupby("tow_speed_nom").mean()    
+    Re_D = df.mean_tow_speed*D/nu
     if normalize_by == "default":
-        norm_cp = cp[-4]
-        norm_cd = cd[-4]
-        norm_cfd = "CFD"
+        norm_cp = df.mean_cp[1.0]
+        norm_cd = df.mean_cd[1.0]
     else:
         norm_cp = normalize_by
         norm_cd = normalize_by
-        norm_cfd = normalize_by
+    plt.figure()
     if errorbars:    
-        plt.errorbar(Re_D, cp/norm_cp, yerr=exp_unc_cp/2/cp[-4], fmt="-ok",
-                     markerfacecolor="none", label="Experiment")
+        plt.errorbar(Re_D, df.mean_cp/norm_cp, yerr=df.exp_unc_cp/norm_cp, 
+                     fmt="-ok", markerfacecolor="none")
     else:
-        plt.plot(Re_D, cp/norm_cp, '-ok', markerfacecolor="none", label="Experiment")
-    if cfd:
-        plot_cfd_perf("cp", normalize_by=norm_cfd)
+        plt.plot(Re_D, df.mean_cp/norm_cp, '-ok', markerfacecolor="none")
     plt.xlabel(r"$Re_D$")
     if normalize_by == "default":
         plt.ylabel(r"$C_P/C_{P0}$")
     else:
         plt.ylabel(r"$C_P$")
-#    plt.ylim((0.4, 1.2))
     ax = plt.gca()
-    plt.grid(True)
     if dual_xaxes:
         plt.text(1.27e6, 1.11, r"$\times 10^5$", color=r"#555555")
         ax2 = ax.twiny()
         ax.xaxis.get_majorticklocs()
         ticklabs = np.arange(0.2e6, 1.6e6, 0.2e6)
-        ticklabs = ticklabs/D*1.9*0.14/1e5
+        ticklabs = ticklabs/D*3.1*0.067/1e5
         ticklabs = [str(np.round(ticklab, decimals=1)) for ticklab in ticklabs]
         ax2.set_xticks(ax.xaxis.get_ticklocs())
         ax2.set_xlim((0.2e6, 1.4e6))
         ax2.set_xticklabels(ticklabs)
 #        ax2.xaxis.major.formatter.set_powerlimits((0,0)) 
         ax2.set_xlabel(r"$Re_{c, \mathrm{ave}}$")
-    if cfd:
-        plt.legend(loc=4)
     ax.xaxis.major.formatter.set_powerlimits((0,0)) 
     plt.tight_layout()
     if save:
         plt.savefig(savedir + "/re_dep_cp" + savetype)
     plt.figure()
     if errorbars:
-        plt.errorbar(Re_D, cd/norm_cd, yerr=exp_unc_cd/cd[-4]/2, fmt="-ok",
-                     markerfacecolor="none", label="Experiment")
+        plt.errorbar(Re_D, df.mean_cd/norm_cd, yerr=df.exp_unc_cd/norm_cd, 
+                     fmt="-ok", markerfacecolor="none")
     else:
-        plt.plot(Re_D, cd/cd[-4], '-ok', markerfacecolor="none", label="Experiment")
+        plt.plot(Re_D, df.mean_cd/norm_cd, '-ok', markerfacecolor="none")
     plt.xlabel(r"$Re_D$")
     if normalize_by == "default":
         plt.ylabel(r"$C_D/C_{D0}$")
     else:
         plt.ylabel(r"$C_D$")
-    plt.hold(True)
-    if cfd:
-        plot_cfd_perf("cd", normalize_by=norm_cfd)
-#    plt.ylim((0.5,1.1))
-    plt.grid(True)
-    if cfd:
-        plt.legend(loc=4)
     ax = plt.gca()
     ax.xaxis.major.formatter.set_powerlimits((0,0)) 
     plt.tight_layout()
